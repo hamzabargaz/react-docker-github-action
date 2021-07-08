31623aa55f24065432113b5ba315ab329e3790b0
Hamza Bargaz <hamza.bargaz@gmail.com>
2021-07-08 18:08:33 +0100

    wip

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,4 +3,12 @@ module.exports = {
     headless: process.env.HEADLESS === true,
     args: ["--no-sandbox"],
   },
+  browser: 'chromium',
+  browserContext: 'default',
+  server: {
+    command: `yarn start`,
+    port: 3000,
+    launchTimeout: 10000,
+    debug: true,
+  },
 };
