8288554e5d036214b3f0835624b2905d6902a212
Hamza Bargaz <hamza.bargaz@gmail.com>
2021-07-09 12:58:01 +0100

    copying repo

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,14 +1,15 @@
+console.log('hello')
 module.exports = {
   launch: {
-    headless: process.env.HEADLESS === true,
-    args: ["--no-sandbox"],
+    dumpio: true,
+    headless: true//process.env.HEADLESS !== 'false',
   },
   browser: 'chromium',
   browserContext: 'default',
   server: {
-    command: `yarn start`,
+    command: `npm start`,
     port: 3000,
     launchTimeout: 10000,
     debug: true,
   },
-};
+}
